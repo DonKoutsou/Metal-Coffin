b712e7687946c77e8a08f2ebd5a1cebf7e5ea17a
DonKoutsou <127870401+DonKoutsou@users.noreply.github.com>
2025-04-02 23:34:06 +0300

    prog

--- Scripts/fan.gd
+++ Scripts/fan.gd
@@ -2,25 +2,23 @@ extends AnimatedSprite2D
 
 class_name Fan
 
-var StartingPos : Vector2
+
 var StartingScale :Vector2
-#var PosToGo : Vector2
-#var AmmWent : float = 0
 
-
-# Variables to control the wiggle effect
-#@export var amplitude: float = 1.0  # Maximum vertical movement
 @export var frequency: float = 1.0  # Wiggle speed
 @export var phase_offset: float = 0.0  # Phase offset for randomness
 @export var max_rotation: float = 0.1  # Maximum angle in radians for rotation
 @export var max_scale_x: float = 1  # Maximum angle in radians for rotation
-#@export var min_scale: float = 0.1  # Maximum angle in radians for rotation
-#@export var skew_factor: float = 0.1  # Maximum skew factor, adjust as needed
+
+@export var BluredSpriteFrames : SpriteFrames
+@export var NonBluredSpriteFrames : SpriteFrames
+
+var On = true
 
 func _ready() -> void:
 	play()
 	get_child(0).play()
-	StartingPos = position
+
 	StartingScale = scale
 	phase_offset = randf_range(0.0, TAU)
 	#PosToGo = position
@@ -47,6 +45,51 @@ func _physics_process(delta: float) -> void:
 	max_scale_x = max(max_scale_x - delta / 60, 0.001)
 	#print("frequency :" + var_to_str(frequency))
 	# Update transformations
-	position = StartingPos
+
 	rotation = rotation_angle
 	scale.x = StartingScale.x * scale_x
+
+var OnOffTween : Tween
+
+func _on_control_gui_input(event: InputEvent) -> void:
+	if (event.is_action_pressed("Click")):
+		if (is_instance_valid(OnOffTween)):
+			OnOffTween.kill()
+		
+		OnOffTween = create_tween()
+		OnOffTween.set_ease(Tween.EASE_OUT)
+		OnOffTween.set_trans(Tween.TRANS_QUAD)
+		if (On):
+			var currentframe = frame
+			var currentframeprog = frame_progress
+			sprite_frames = NonBluredSpriteFrames
+			play()
+			frame = currentframe
+			frame_progress = currentframeprog
+			On = false
+			OnOffTween.tween_method(SetFps, 24, 0, 2)
+		else:
+			var currentframe = frame
+			var currentframeprog = frame_progress
+			sprite_frames = BluredSpriteFrames
+			
+			play()
+			frame = currentframe
+			frame_progress = currentframeprog
+			On = true
+			OnOffTween.tween_method(SetFps, 0, 24, 1)
+
+func SetFps(FPS: int) -> void:
+	sprite_frames.set_animation_speed("default", FPS)
+	$AnimatedSprite2D.sprite_frames.set_animation_speed("default", FPS)
+	var pitch = FPS / 24.0
+	if (pitch == 0):
+		$AudioStreamPlayer2D.stop()
+		return
+	if (!$AudioStreamPlayer2D.playing):
+		$AudioStreamPlayer2D.play()
+	$AudioStreamPlayer2D.pitch_scale = pitch
+
+
+func _on_visibility_changed() -> void:
+	$AudioStreamPlayer2D.playing = is_visible_in_tree()
